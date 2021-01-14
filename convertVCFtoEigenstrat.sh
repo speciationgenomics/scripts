eee3ce94b0898c6713800affcad10b4cead45895
Joana <joana.isabel.meier@gmail.com>
2021-01-14 18:18:05 +0100

    Add option to rename scaffolds
    A new option was added to rename scaffolds to "1" and make the positions cumulative (concatenating all scaffolds). This is useful for datasets based on reference genomes that are not chromosome level assemblies. The script now also assumes a constant recombination rate of 2 cM/Mb and uses the SNP positions to compute the Morgan positions.

--- convertVCFtoEigenstrat.sh
+++ convertVCFtoEigenstrat.sh
@@ -2,19 +2,41 @@
 
 # Script to convert vcf to eigenstrat format for ADMIXTOOLS
 # Written by Joana Meier
-# It takes a single argument: the vcf file (can be gzipped)
+# It takes a single argument: the vcf file (can be gzipped) and 
+# optionally you can specify --renameScaff if you have scaffold names (not chr1, chr2...)
+# The script assumes a constant recombination rate of 2 cM/Mb. You may have to change that or change the jackknife block size in ADMIXTOOLS.
+
+# Here, you can change the recombination rate which is currently set to 2 cM/Mb 
+rec=2
 
 # It requires vcftools and admixtools
 
 # for some clusters, it is needed to load these modules:
 # module load gcc/4.8.2 vcftools openblas/0.2.13_seq perl/5.18.4 admixtools
 
-if [ $# -ne 1 ]
- then
- echo "Please provide the vcf file to parse"
+renameScaff="FALSE"
+
+# If help is requested
+if [[ $1 == "-h" ]]
+then
+ echo "Please provide the vcf file to parse, and optionally add --renameScaff if you have scaffolds instead of chromosomes"
+ echo "Usage: convertVCFtoEigenstrat.sh <vcf file> --renameScaff --rec 2 (note, the second argument is optional)"
+ exit 1
+
+# If the second argument renameScaff is given, set it to True
+elif [[ $2 == "--renameScaff" ]]
+then
+ renameScaff="TRUE"
+
+# If no argument is given or the second one is not -removeChr, give information and quit
+elif [ $# -ne 1 ]
+then
+ echo "Please provide the vcf file to parse, and optionally add --renameScaff if you have scaffolds instead of chromosomes"
+ echo "Usage: ./convertVCFtoEigenstrat.sh <vcf file> --renameScaff (note, the second argument is optional)"
  exit 1
 fi
 
+# Set the first argument to the file name
 file=$1
 file=${file%.gz}
 file=${file%.vcf}
@@ -25,26 +47,62 @@ file=${file%.vcf}
 if [ -s $file.vcf.gz ]
 then
 
- # Get a .map and .ped file
+ # If renaming of scaffolds is requested, set all chromosome/scaffold names to 1
+ if [ $renameScaff == "TRUE" ]
+ then
+  echo "setting scaffold names to 1 and positions to additive numbers"
+  zcat $file".vcf.gz" | awk 'BEGIN {OFS = "\t";add=0;lastPos=0;scaff=""}{
+    if($1!~/^#/){
+       if($1!=scaff){add=lastPos;scaff=$1}
+       $1="1"
+       $2=$2+add
+       lastPos=$2
+     }
+     print $0}' | gzip > $file.renamedScaff.vcf.gz
+
+  # Get a .map and .ped file (remove multiallelic SNPs, monomorphic sites and indels)
+  vcftools --gzvcf $file".renamedScaff.vcf.gz" \
+         --plink --mac 1.0 --remove-indels --max-alleles 2 --out $file
+
+ else
+ # Get a .map and .ped file (remove multiallelic SNPs, monomorphic sites and indels)
  vcftools --gzvcf $file".vcf.gz" \
          --plink --mac 1.0 --remove-indels --max-alleles 2 --out $file
+ fi
 
+# if the file is not gzipped
 else
-vcftools --vcf $file".vcf" \
-         --plink --mac 1.0 --remove-indels --max-alleles 2 --out $file
+ # If renaming of scaffolds is requested, set all chromosome/scaffold names to 1
+ if [ $renameScaff == "TRUE" ]
+ then
+  echo "setting scaffold names to 1 and positions to additive numbers"
+  awk 'BEGIN {OFS = "\t";add=0;lastPos=0;scaff=""}{
+    if($1!~/^#/){
+       if($1!=scaff){add=lastPos;scaff=$1}
+       $1="1"
+       $2=$2+add
+       lastPos=$2
+     }
+     print $0}' $file.vcf | gzip > $file.renamedScaff.vcf.gz
 
+  # Get a .map and .ped file (remove multiallelic SNPs, monomorphic sites and indels)
+  vcftools --gzvcf $file".renamedScaff.vcf.gz" \
+         --plink --mac 1.0 --remove-indels --max-alleles 2 --out $file
+ else
+ vcftools --vcf $file".vcf" --plink --mac 1.0 --remove-indels --max-alleles 2 --out $file
+ fi
 fi
 
 
-# Change the .map file to match the requirements of ADMIXTOOLS
-awk -F"\t" 'BEGIN{scaff="";add=0}{
+# Change the .map file to match the requirements of ADMIXTOOLS by adding fake Morgan positions (assuming a recombination rate of 2 cM/Mbp)
+awk -F"\t" -v rec=$rec 'BEGIN{scaff="";add=0}{
         split($2,newScaff,":")
         if(!match(newScaff[1],scaff)){
                 scaff=newScaff[1]
                 add=lastPos
         }
-        count+=0.0001
         pos=add+$4
+	       count+=0.00000001*rec*(pos-lastPos)
         print newScaff[1]"\t"$2"\t"count"\t"pos
         lastPos=pos
 }' ${file}.map  | sed 's/^chr//' > better.map
